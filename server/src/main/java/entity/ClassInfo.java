d598a75432ee18abcea2afbaa90ec47fc6f10380
audeSt <audestick@gmail.com>
2016-11-24 22:06:53 +0800

    init

---
+++ server/src/main/java/entity/ClassInfo.java
@@ -0,0 +1,18 @@
+package entity;
+
+import java.io.Serializable;
+
+/**
+ * Created by lrkin on 2016/11/24.
+ */
+public class ClassInfo implements Serializable {
+
+    private String className;
+
+    private String classPath;
+
+    private String methodName;
+
+    private String methodParam;
+    
+}
---
+++ server/src/main/java/netty/NettyServerHandler.java
@@ -0,0 +1,7 @@
+package netty;
+
+/**
+ * Created by lrkin on 2016/11/24.
+ */
+public class NettyServerHandler {
+}
