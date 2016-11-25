0f0a445b305781f7df6973f7521c13c34b8ec982
audeSt <audestick@gmail.com>
2016-11-26 00:07:06 +0800

    update

--- server/src/main/java/netty/NettyServerHandler.java
+++ server/src/main/java/netty/NettyServerHandler.java
@@ -1,9 +1,15 @@
 package netty;
 
 import entity.AudeRequest;
+import entity.ClassInfo;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.internal.StringUtil;
 import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by lrkin on 2016/11/24.
@@ -25,6 +31,20 @@ public class NettyServerHandler extends ChannelHandlerAdapter {
         AudeRequest request = (AudeRequest) msg;
         logger.info("Receive the aude request: {}", request);
 
-        
+        //默认执行成功
+        Boolean operSuccess = true;
+        //执行结果字符串
+        StringBuilder resultSB = new StringBuilder();
+
+        Set<ClassInfo> classInfoSet = request.getClasses();
+
+        if (classInfoSet != null && classInfoSet.size() > 0) {
+            //建立线程池准备执行
+            ExecutorService threadPool = Executors.newFixedThreadPool(classInfoSet.size());
+            for (final ClassInfo classInfo : classInfoSet) {
+                if (classInfo != null && StringUtils.isNotBlank(classInfo.getClassName())) {
+                }
+            }
+        }
     }
 }
